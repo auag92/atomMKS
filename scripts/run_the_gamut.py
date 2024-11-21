2294b50c726eab99d9b865b43b8b4559dd3953ab
apaar.shanker <apaar.shanker@scale.com>
2024-11-21 19:32:36 +0000

    run_the_gamut improved

--- scripts/run_the_gamut.py
+++ scripts/run_the_gamut.py
@@ -1,38 +1,46 @@
-import numpy as np
+from pathlib import Path
+
 import ase.io as aio
+import numpy as np
 from toolz.curried import pipe
-import atommks.porosity as pore
+
 import atommks.grid_generator as gen
+import atommks.porosity as pore
 
 
 def generate_pore_metrics(
     fname,
     radii={"Si": 1.35, "O": 1.35, "H": 1.0},
     len_pixel=10,
-    r_probe=0.1, # probe radius
+    r_probe=0.1,  # probe radius
 ):
     """
     Saves the voxelized structure in matfile format
     """
     try:
 
-
-        rep=[2, 2, 1]
+        rep = [2, 2, 1]
         padval = ((1, 1), (1, 1), (0, 0))
 
         S = pipe(
             fname,
             lambda f: aio.read(f).repeat(rep),
-            lambda a: gen.grid_maker(a, len_pixel=10, atomic_radii=radii, full=False, fft=True)[0],
+            lambda a: gen.grid_maker(
+                a, len_pixel=10, atomic_radii=radii, full=False, fft=True
+            )[0],
             lambda s: np.pad(s, padval, "constant", constant_values=0),
             lambda s: pore.dgrid(s, len_pixel),
         )
 
-        pld = pore.get_pld(S) # calculates the pore limiting diameter, a scaler
-        
-        lcd = pore.get_lcd(S) # calculates the largest cavity diameter, a scaler
+        pld = pore.get_pld(
+            S
+        )  # calculates the pore limiting diameter, a scaler
 
-        # generates probe accessible pore region [grid representation] 
+        lcd = pore.get_lcd(
+            S
+        )  # calculates the largest cavity diameter, a scaler
+
+        # generates probe accessible pore region [grid representation]
         S_1 = (
             pore.gen_cleanPore(
                 S, r_probe=r_probe, r_min=2.5, len_pixel=len_pixel
@@ -40,7 +48,7 @@ def generate_pore_metrics(
             > 0
         ) * 1
 
-        # generates medial axis of the accessible pore region [grid representation]
+        # generates medial axis of the accessible pore region [grid representation] # noqa: E501
         S_2 = pipe(
             S_1,
             lambda x: np.pad(
@@ -70,15 +78,29 @@ def generate_pore_metrics(
         # dimensions of the structure
         dim = np.asarray(S.shape) / len_pixel
 
-        print(fname, pld, lcd, asa, av, n_paths, np.mean(paths), np.mean(psd), dim)
+        print(
+            fname,
+            pld,
+            lcd,
+            asa,
+            av,
+            n_paths,
+            np.mean(paths),
+            np.mean(psd),
+            dim,
+        )
 
     except Exception as err:
         print("Exception for file : %s" % (fname), err)
 
 
 if __name__ == "__main__":
+
+    cif = "MFI.cif"
+    fname = Path.cwd().parents[0] / "datasets" / "iza_zeolites" / cif
+
     generate_pore_metrics(
-        fname="../datasets/iza_zeolites/ABW.cif",
+        fname=fname,
         radii={"Si": 1.35, "O": 1.35, "H": 0.5},
         len_pixel=10,
         r_probe=0.5,
